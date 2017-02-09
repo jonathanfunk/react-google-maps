48694e2ae6a5e2a70b5b720b99ace2994c224e12
Jonathan Funk <jonfunk88@hotmail.com>
2017-02-09 14:32:47 -0800

    all redux files moved to redux folder

---
+++ src/Redux/store.js
@@ -0,0 +1,47 @@
+import {createStore} from 'redux';
+import { features } from './../mock-data'//Temporary!!!
+import reducer from './reducer'
+
+export const store = createStore(reducer)
+
+// convert json into dict for use by the React components
+// add mapOn variable to indicate if the marker should be visible
+// by default, set mapOn to false, filtering will indicate if it should be true
+function get_campgrounds(features) {
+  let campgrounds = []
+  features.forEach(feature => {
+    campgrounds.push({
+      'title' : feature['properties']['title'],
+      'description' : feature['properties']['description'],
+      'position' : [feature['geometry']['coordinates'][1],
+      feature['geometry']['coordinates'][0]],
+      'properties': feature['properties'],
+      'image': feature['properties']['image'],
+      'url': feature['properties']['url'],
+      'mapOn': true
+
+    })
+  });
+  return campgrounds
+}
+
+set_state(get_campgrounds(features))
+
+function set_state(campgrounds) {
+  store.dispatch ({
+  type: 'SET_STATE',
+  state: {
+    filters: [
+      {id: 'shower', inuse: false },
+      {id: 'pets', inuse: false },
+      {id: 'flush', inuse: false },
+      {id: 'water', inuse: false }
+    ],
+    markers: campgrounds,
+    gmapMarkers: [],
+    showingInfoWindow: "false",
+    activeMarker: null,
+    selectedTitle: ""
+  }
+ })
+}
